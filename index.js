a1cad6802fbc50ff768bdf60ebdce9b9131a25ba
JuanMa Dolce <jmdolce@hotmail.com>
2022-06-12 22:56:15 -0300

    Classes

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-const usuario = {}
+class Usuario {
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.libros = [];
+        this.mascotas = [];
+    }
+
+    getFullName(){ 
+            return `Hola ${this.nombre} ${this.apellido}`
+        }
+
+    addBook(nombre,autor){
+        this.libros.push({
+            nombre: nombre,
+            autor: autor
+        })
+    }
+    getBookNames(){
+        const bookNames = this.libros.map(x => x.nombre)
+        return bookNames
+    }
+
+    addMascota(mascota){
+        this.mascotas.push(mascota)
+    }
+    countMascotas(){
+        return this.mascotas.length
+    }
+}
+
+const usuario = new Usuario('Juan Manuel','Dolce')
+
+console.log(usuario.getFullName())
+
+usuario.addBook('El Señor de Los Anillos', 'JRR Tolkien')
+usuario.addBook('La Torre Oscura', 'Stephen King')
+
+console.log(usuario.getBookNames())
+
+usuario.addMascota('Perro')
+usuario.addMascota('Loro')
+
+console.log(usuario.countMascotas())
